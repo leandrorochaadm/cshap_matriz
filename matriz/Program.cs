c5466bd4559e22090972112caabba2a0311006c7
leandrorochaadm <leandrorochaadm@gmail.com>
2020-09-27 23:40:57 -0300

    funcao pra criar matriz

---
+++ matriz/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace matriz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("**Matrizes**");
+            Console.WriteLine("Quantas linhas deseja?");
+            int linhas = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Quantas linhas deseja?");
+            int colunas = int.Parse(Console.ReadLine());
+
+            int[,] Matriz1 = new int[linhas, colunas];
+
+            for (int i = 0; i < linhas; i++)
+            {
+
+                for (int j = 0; j < colunas; j++)
+                {
+                    Console.WriteLine($"Informe o valor a ser inserido na matriz [{i+1},{j+1}]");
+                    Matriz1[i, j] = Convert.ToInt32(Console.ReadLine());
+                }
+
+            }
+
+            Console.WriteLine("---> Imprimindo elementos  <---");
+
+
+            for (int i = 0; i < linhas; i++)
+            {
+                for (int j = 0; j < colunas; j++)
+                {
+                    Console.WriteLine($"Matriz [{i+1}, {j+1}] = {Matriz1[i,j]}");
+                }
+            }
+        }
+    }
+}
